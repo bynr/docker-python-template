89d473af4109caefce4b0891224e32489a116b7c
bynr <remy.bin+git@gmail.com>
2021-10-10 18:28:37 +0900

    Add a sample notebook

--- src/play/main.py
+++ src/play/main.py
@@ -2,17 +2,11 @@ import pandas as pd
 
 
 def do_a():
-    a = pd.DataFrame()
+    a = pd.DataFrame([1])
     b = 123
     return a
 
 
-def run(a, b) -> pd.DataFrame:
-    c = pd.DataFrame
-    return c
-
-
 if __name__ == "__main__":
-    run()
     a = do_a()
     print(a)
